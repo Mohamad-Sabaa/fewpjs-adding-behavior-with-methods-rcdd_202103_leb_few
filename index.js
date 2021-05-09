2fbe086dfd078ab5e9a2a4f21174e09527811c3a
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-05-09 14:22:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ class Bird {
     this.sex = sex;
   }
   speak() {
-    return sex === male ? `it's me ${this.name}, the parrot` : `${this.name} says squawk`
+    return this.sex === male ? `it's me ${this.name}, the parrot` : `${this.name} says squawk`
   }
 }
